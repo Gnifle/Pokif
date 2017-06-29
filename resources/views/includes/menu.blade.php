597bf1ffcc3cf7f14ed55ba9301778333c35fcad
Daniel Gnifle <github@gnifle.com>
2017-06-29 23:20:30 +0200

    Improvements and percent-widths to menu.

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -2,15 +2,39 @@
 	<li>
 		<a href="/pokedex">
 			<svg>
-				<use xlink:href="#open-pokeball"></use>
+				<use xlink:href="#camera"></use>
 				<span>Pokedex</span>
 			</svg>
 		</a>
 	</li>
 	<li>
-		<a href="/pokedex">
+		<a href="/abilitydex">
+			<svg>
+				<use xlink:href="#up-arrow"></use>
+				<span>Items</span>
+			</svg>
+		</a>
+	</li>
+	<li>
+		<a href="/attackdex">
+			<svg>
+				<use xlink:href="#fist"></use>
+				<span>Items</span>
+			</svg>
+		</a>
+	</li>
+	<li>
+		<a href="/itemdex">
 			<svg>
 				<use xlink:href="#backpack"></use>
+				<span>Items</span>
+			</svg>
+		</a>
+	</li>
+	<li>
+		<a href="/areadex">
+			<svg>
+				<use xlink:href="#map"></use>
 				<span>Items</span>
 			</svg>
 		</a>
